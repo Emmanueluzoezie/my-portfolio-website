7d3779bc1830537e033db0474189b7bcd9f453dc
emmanuel uzoezie <jonueldev@gmail.com>
2022-05-02 18:06:00 +0100

    reduce the px in the project page

--- components/project.js
+++ components/project.js
@@ -10,8 +10,8 @@ function Project (){
     return(
         <div className=" my-5 ">
             <h1 className="text-center text-2xl dark:text-white text-black font-bold font-mono">My Projects</h1>
-            <div className=" md:flex md:space-x-7 px-14">
-            <div className="my-8 border pb-2 bg-gray-300 dark:bg-gray-600 ">
+            <div className=" md:flex md:space-x-7 px-10">
+            <div className="my-8 border pb-2 bg-gray-300 shadow-xl dark:bg-gray-600 ">
                 <Link href="https://coins-tracker.vercel.app/" passHref>
                     <div className="cursor-pointer">
                         <Image
@@ -21,7 +21,7 @@ function Project (){
                     </div>  
                 </Link>
                 
-                <div className=" flex justify-between cursor-pointer px-4">
+                <div className=" flex justify-between cursor-pointer px-4 ">
                     <Link  href="https://github.com/Emmanueluzoezie/coin-tracker" passHref>
                     <a className="dark:text-white text-black font-semibold hover:text-blue-400 dark:hover:text-blue-600">Get the source code</a>
                     </Link>
@@ -30,7 +30,7 @@ function Project (){
                     </Link>
                 </div>
             </div>
-            <div className="my-8 border pb-2 bg-gray-300 dark:bg-gray-600 ">
+            <div className="my-8 border pb-2 bg-gray-300 dark:bg-gray-600 shadow-xl ">
                 <Link href="https://googl-clone-site.netlify.app/" passHref>
                     <div className="cursor-pointer">
                         <Image
--- pages/projects.js
+++ pages/projects.js
@@ -20,15 +20,3 @@ const Projects = () => {
 }
 
 export default Projects;
-
-// <div className="grid place-items-center h-4/6 ">
-//       <div className=" grid place-items-center ">
-//                 <div className=" rounded-full hover:ring-[5px] w-52 h-22 hover:w-64 hover:h-64 lg:hover:w-96 lg:hover:h-96 lg:w-80 lg:h-80 md:w-60 md:h-60 md:hover:w-72 md:hover:h-72">
-//                     <Image
-//                         className=" animate-pulse rounded-full w-full object-cover"
-//                         src={logo}
-//                         alt="Emmanuel uzoezie"
-//                      />
-//                 </div>
-//                 </div>
-//       </div>
