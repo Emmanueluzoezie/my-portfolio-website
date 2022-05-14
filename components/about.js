c7a120086b66883a6acbf3b230066c211572a09b
emmanuel uzoezie <jonueldev@gmail.com>
2022-05-14 15:57:30 +0100

    fixing some typos

--- components/about.js
+++ components/about.js
@@ -8,11 +8,11 @@ export default function About() {
                     <h1 className="text-center text-2xl font-bold font-mono">
                         Know Who <span> I Am </span>
                     </h1>
-                    <p className="text-lg mb-4"><span className="text-xl mr-1 font-semibold">I</span>am Emmanuel uzoezie, am a frontend developer. I have 2+ years expereince as junior software developer. Have work on some Frontend development jobs, checkout the project page to see some of what i have done so far <a href="https://emmanueluzoezie.bio.link/" className="hover:text-blue-600 hover:underline text-lg">link to social and blog pages.</a></p>
+                    <p className="text-lg mb-4"><span className="text-xl mr-1 font-semibold">I</span>am Emmanuel uzoezie, am a frontend developer.  having over 2+ years of experience in junior software Industry. Have work on some Frontend technologies, checkout the project page to see some of the projects have done so far <a href="https://emmanueluzoezie.bio.link/" className="hover:text-blue-600 hover:underline text-lg">social-links.</a></p>
                     <h1 className="text-center text-2xl font-bold font-mono">
                         Hobbies
                     </h1>
-                    <p className="text-lg mb-4"><span className="text-xl font-semibold mr-1">I</span>love playing football, reading, coding and Exercise</p>
+                    <p className="text-lg mb-4"><span className="text-xl font-semibold mr-1">I</span>love coding, watching football and exercise with alot of passion about technolgy.</p>
                 </div>
             </section>
         </div>
